b7e3439c882a337f3679509726f9652b96248d29
Victor Cruz <vfcruz07@gmail.com>
2021-05-18 09:08:18 -0300

    Projeto Criado

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+clima = "quente";
+
+pessoa = {
+    name:'victor',
+    idade:'29',
+    andar: function(){
+        return(console.log('andou'));
+    }
+}
+
+individuo = [
+    'victor cruz',
+    29
+]
+
+
+console.log(pessoa.name);
+console.log(pessoa.andar());
+console.log(individuo[0]);
+console.log(clima);
