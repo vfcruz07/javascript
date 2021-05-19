2bcf0e13d5fed0b343d064ade11bf702b9576d26
Victor Cruz <vfcruz07@gmail.com>
2021-05-19 14:53:11 -0300

    Function Scope

---
+++ functions.js
@@ -0,0 +1,20 @@
+function createPhrases(){
+    console.log("Frase 1");
+    console.log("Frase 2");
+    console.log("Frase 3");
+}
+
+//createPhrases();
+
+
+const sum = function(number1, number2){
+    let total = number1 + number2;
+    return total;
+}
+
+let number1 = 25;
+let number2 = 34;
+
+console.log(`Valor 1: ${number1}`);
+console.log(`Valor 2: ${number2}`);
+console.log(`Soma: ${sum(number1, number2)}`);
